ff6e2f526f444ee7d311eef3ce4019cb0c61d314
megan-k-ayers <m.ayers@yale.edu>
2024-08-19 18:24:25 -0400

    Cleaned up core tuning code

--- R/tune_model.R
+++ R/tune_model.R
@@ -1,3 +1,64 @@
+#' Get Parameter List from Grid Row
+#'
+#' @param grid_row Grid row of parameter settings to evaluate
+#'
+#' @return A list
+#'
+#' @examples
+get_row_list <- function(grid_row) {
+  return(list("n_filts" = unlist(grid_row$n_filts),
+              "kern_sizes" = unlist(grid_row$kern_sizes),
+              "lr" = unlist(grid_row$lr),
+              "lambda_cnn" = unlist(grid_row$lambda_cnn),
+              "lambda_corr" = unlist(grid_row$lambda_corr),
+              "lambda_out" = unlist(grid_row$lambda_out),
+              "epochs" = unlist(grid_row$epochs),
+              "batch_size" = unlist(grid_row$batch_size),
+              "patience" = unlist(grid_row$patience),
+              "covars" = unlist(grid_row$covars)))
+}
+
+
+#' Get Document-Level Activation Metrics (tuning)
+#'
+#' @inheritParams get_doc_acts
+#'
+#' @return Data frame with metrics.
+#' @export
+#'
+#' @examples
+get_doc_metrics <- function(model, params, embeds, dat) {
+  # Document-level activation ranges
+  acts <- get_doc_acts(model = model, params = params, embeds = embeds,
+                       dat = dat)
+  ranges <- stats::aggregate(activation ~ filter, acts, range)
+  ranges <- round(ranges$activation[, 2] - ranges$activation[, 1], 3)
+  ranges <- paste(ranges, collapse = "|")
+
+  # Max correlation between document-level activations
+  acts <- tidyr::pivot_wider(acts[, c("filter", "activation", "sample_id")],
+                             names_from = "filter", id_cols = "sample_id",
+                             values_from = "activation")
+  acts <- acts[, 2:ncol(acts)]
+  max_corr <- max(stats::cor(acts) - diag(nrow = ncol(acts)))
+  return(data.frame("act_range" = ranges, "max_corr" = max_corr))
+}
+
+
+#' Tuning Wrapper for get_top_phrases_quick()
+#'
+#' @inheritParams get_top_phrases_quick
+#'
+#' @return Short summary of top phrases
+#'
+#' @examples
+get_phrase_metrics <- function(model, params, embeds, tokens, vocab) {
+  acts <- get_phrase_acts(model = model, params = params, embeds = embeds)
+  return(get_top_phrases_quick(phrase_acts = acts,  tokens = tokens,
+                               params = params, vocab = vocab))
+}
+
+
 #' Tune ConTextNet Model
 #'
 #' @param dat Original text data set with outcome `y`
@@ -8,12 +69,10 @@
 #' @param vocab Vocab map from tokenizer
 #'
 #' @return Data frame, which is grid with model performances filled in.
-#' @export
 #'
 #' @examples
 tune_model <- function(dat, embeds, meta_params, grid, tokens, vocab) {
   ### TODO: Incorporate tuning progress into future log system.
-  ### TODO: Need to shuffle tune_fold, shouldn't be ordered.
   ### TODO: Figure out if memory clearing is possible without adverse user-side
   ### effects?
 
@@ -31,17 +90,7 @@ tune_model <- function(dat, embeds, meta_params, grid, tokens, vocab) {
   for (i in 1:nrow(grid)) {
 
     # Grab parameter settings for this grid row.
-    these_params <- list("n_filts" = unlist(grid$n_filts[i]),
-                         "kern_sizes" = unlist(grid$kern_sizes[i]),
-                         "lr" = unlist(grid$lr[i]),
-                         "lambda_cnn" = unlist(grid$lambda_cnn[i]),
-                         "lambda_corr" = unlist(grid$lambda_corr[i]),
-                         "lambda_out" = unlist(grid$lambda_out[i]),
-                         "epochs" = unlist(grid$epochs[i]),
-                         "batch_size" = unlist(grid$batch_size[i]),
-                         "patience" = unlist(grid$patience[i]),
-                         "covars" = unlist(grid$covars[i]))
-    these_params <- c(meta_params, these_params)
+    these_params <- c(meta_params, get_row_list(grid[i, ]))
 
     # Switch up test and train folds for cross-validation, train model.
     temp_dat$fold <- ifelse(dat$tune_fold == grid$run[i], "test", "train")
@@ -57,35 +106,22 @@ tune_model <- function(dat, embeds, meta_params, grid, tokens, vocab) {
     grid[i, cols] <- eval_model(model, embeds[temp_dat$fold == "test", , ],
                                 temp_dat$y[temp_dat$fold == "test"], metrics)
 
-
-    # Evaluate spread of the filter activations on the validation samples,
-    # record this in grid as well.
-    acts <- get_doc_acts(model = model, params = these_params,
-                         embeds = embeds[temp_dat$fold == "test", , ],
-                         dat = temp_dat[temp_dat$fold == "test", ])
-
-    ranges <- stats::aggregate(activation ~ filter, acts, range)
-    ranges <- round(ranges$activation[, 2] - ranges$activation[, 1], 3)
-    grid$act_range[i] <- paste(ranges, collapse = "|")
-
-    # Also record the max correlation between document-level filter activations.
-    acts <- tidyr::pivot_wider(acts[, c("filter", "activation", "sample_id")],
-                               names_from = "filter", id_cols = "sample_id",
-                               values_from = "activation")
-    acts <- acts[, 2:ncol(acts)]
-    grid$max_corr[i] <-  max(stats::cor(acts) - diag(nrow = ncol(acts)))
+    # Record spread of the filter activations on the validation samples and
+    # max correlation between document-level filter activations.
+    grid[i, c("act_range", "max_corr")] <- get_doc_metrics(model = model,
+                                   params = these_params,
+                                   embeds = embeds[temp_dat$fold == "test", , ],
+                                   dat = temp_dat[temp_dat$fold == "test", ])
 
     # Finally, return very short summary of filter interpretations on the
     # validation set.
-    p_acts <- get_phrase_acts(model = model, params = these_params,
-                              embeds = embeds[temp_dat$fold == "test", , ])
-    grid$phrases[i] <- get_top_phrases_quick(phrase_acts = p_acts,
-                                             tokens = tokens,
-                                             params = these_params,
-                                             vocab = vocab)
+    grid$phrases[i] <- get_phrase_metrics(model = model, params = these_params,
+                                  embeds = embeds[temp_dat$fold == "test", , ],
+                                  tokens = tokens[temp_dat$fold == "test", ],
+                                  vocab = vocab)
 
     # Free up memory (only partially successful in my past experience)
-    rm(list = c("p_acts", "acts", "model"))
+    rm(list = c("model"))
     keras::k_clear_session()
     tf$keras$backend$clear_session()
     gc()
